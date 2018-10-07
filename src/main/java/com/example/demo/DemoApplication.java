9c3650800465ea0dabeb468692e1684c1d444442
RestartCoding <32931558+RestartCoding@users.noreply.github.com>
2018-10-07 23:51:26 +0800

    make a mistake
    make a mistak

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+                11111111111;
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
